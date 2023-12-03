ee547bedc1a2d87fdd6aebf4bcdf8083bc8b13c7
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:49:50 +0900

    feat: add resource internet gateway

--- module-network/main.tf
+++ module-network/main.tf
@@ -79,3 +79,12 @@ resource "aws_subnet" "private-subnet-d" {
   }
 }
 
+# igw
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.vpc_name}-igw"
+  }
+}
+
