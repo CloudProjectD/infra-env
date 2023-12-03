b18fe395449f87109903d7d495e523f9dc5c8021
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:50:57 +0900

    feat: add resource bastion host security group

--- module-network/main.tf
+++ module-network/main.tf
@@ -112,3 +112,24 @@ resource "aws_route_table_association" "public-b-association" {
   route_table_id = aws_route_table.public-route.id
 }
 
+# bastion sg
+resource "aws_security_group" "bastion-sg" {
+  name        = "bastion-sg"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
