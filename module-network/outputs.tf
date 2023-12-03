8543fc4371d47e9a516c444b153ad697d717efe2
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:44:57 +0900

    feat: set network outputs.tf

---
+++ module-network/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = [
+    aws_subnet.public-subnet-a.id,
+    aws_subnet.public-subnet-b.id,
+    aws_subnet.private-subnet-a.id,
+    aws_subnet.private-subnet-b.id,
+    aws_subnet.private-subnet-c.id,
+    aws_subnet.private-subnet-d.id,
+  ]
+}
+
+output "asg_subnet_ids" {
+  value = [
+    aws_subnet.private-subnet-a.id,
+    aws_subnet.private-subnet-b.id,
+  ]
+}
+
+output "db_subnet_ids" {
+  value = [
+    aws_subnet.private-subnet-c.id,
+    aws_subnet.private-subnet-d.id,
+  ]
+}
+
+output "public_subnet_ids" {
+  value = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
+}
