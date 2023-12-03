c5ee2aeca291c0706cd1a6e420581480cb76e0f0
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:51:32 +0900

    feat: add private ec2 instances security group

--- module-network/main.tf
+++ module-network/main.tf
@@ -133,3 +133,23 @@ resource "aws_security_group" "bastion-sg" {
   }
 }
 
+# private ec2 sg
+resource "aws_security_group" "private-sg" {
+  name        = "private-sg"
+  description = "Allow inbound traffic for private ec2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
