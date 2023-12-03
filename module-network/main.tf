fe0154d8f8ad288dce631498d1e8f16c3cbb51dd
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:50:22 +0900

    feat: add resource public routing

--- module-network/main.tf
+++ module-network/main.tf
@@ -88,3 +88,27 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# public route
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "public-a-association" {
+  subnet_id      = aws_subnet.public-subnet-a.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+resource "aws_route_table_association" "public-b-association" {
+  subnet_id      = aws_subnet.public-subnet-b.id
+  route_table_id = aws_route_table.public-route.id
+}
+
