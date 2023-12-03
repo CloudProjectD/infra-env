08600c19fa3da5b58b619d2340045da9c5f6e197
dusdjhyeon <dusdj0813@gmail.com>
2023-12-03 19:48:20 +0900

    feat: add resource subnet

--- module-network/main.tf
+++ module-network/main.tf
@@ -14,3 +14,68 @@ resource "aws_vpc" "main" {
   }
 }
 
+# subnet
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public-subnet-a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-public-subnet-a")
+  }
+}
+
+resource "aws_subnet" "public-subnet-b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-public-subnet-b")
+  }
+}
+
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-private-subnet-a")
+  }
+}
+
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-private-subnet-b")
+  }
+}
+
+resource "aws_subnet" "private-subnet-c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_c_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-private-subnet-c")
+  }
+}
+
+resource "aws_subnet" "private-subnet-d" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_d_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name" = ("${local.vpc_name}-private-subnet-d")
+  }
+}
+
